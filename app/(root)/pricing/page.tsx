8bc46c6ff8f1f2b8394d8025fc3fc1bd337f5eda
Somesh Dwivedi <116994984+Somesh0001@users.noreply.github.com>
2024-04-06 03:48:01 +0530

    minor fix

--- app/(root)/pricing/page.tsx
+++ app/(root)/pricing/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import tick from "@/assets/tick.jpg";
-import cross from "@/assets/cross.jpg";
-import Image from "next/image";
 import Navbar from "../dashboard/Navbar";
 import { useState } from "react";
 import Pricing from "./Pricing";
-import About from "../dashboard/About";
 // import Hero from "./Hero";
 
 export default function Page() {
