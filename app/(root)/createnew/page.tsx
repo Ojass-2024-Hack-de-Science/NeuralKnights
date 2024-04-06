ca6635f7eb0357574b3afab6d23a500ce65723ef
unnati <unnatichaurasia12@gmail.com>
2024-04-06 11:51:51 +0530

    minor changes

--- app/(root)/createnew/page.tsx
+++ app/(root)/createnew/page.tsx
@@ -14,7 +14,7 @@ import { useRouter } from "next/navigation";
 import { useAnimate } from "framer-motion";
 import { title } from "process";
 const CreateNewBlog = () => {
-  const [isPending,startTransition] = useTransition()
+  const [isPending, startTransition] = useTransition();
   const form = useForm<z.infer<typeof blogZod>>({
     resolver: zodResolver(blogZod),
     defaultValues: {
@@ -24,17 +24,19 @@ const CreateNewBlog = () => {
   });
   const router = useRouter();
   async function onSubmit(values: z.infer<typeof blogZod>) {
-    if(values.title==="" || values.content==="") return null
-    const data = await axios.post("http://localhost:3000/api/newBlog",{title:values.title,content:values.content})
-    router.push("/blog")
+    if (values.title === "" || values.content === "") return null;
+    const data = await axios.post("http://localhost:3000/api/newBlog", {
+      title: values.title,
+      content: values.content,
+    });
+    router.push("/blog");
   }
 
   return (
-
     <div className=" flex flex-col p-28 min-h-[100vh] w-[100vw] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]  from-blue-200 to-blue-400">
       <Link href={"/blog"}>
         <div className="absolute top-9 left-9  font-semibold text-xl flex justify-center items-center gap-2">
-          <div>Create Blog</div>
+          <div>Back to Community Forum</div>
         </div>
       </Link>
       <Form {...form}>
